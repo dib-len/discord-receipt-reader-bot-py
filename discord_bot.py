ceea4163cbb047f6c033c8f96c9ca14931d879ce
Sinan <psydexx64@gmail.com>
2023-04-12 23:48:07 +0100

    feat: add hello command

--- discord_bot.py
+++ discord_bot.py
@@ -3,7 +3,15 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
 load_dotenv()
 token = os.getenv('BOT_TOKEN')
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-print(token)
+@bot.command(name='hello')
+async def say_hello(ctx):
+    await ctx.send('Hello!')
+
+bot.run(token)
