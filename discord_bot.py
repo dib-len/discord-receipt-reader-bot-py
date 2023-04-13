4e4847eefcd9ad9b327696fbf2796c64ff317330
Sinan <psydexx64@gmail.com>
2023-04-13 23:05:12 +0100

    feat: add tesseract

--- discord_bot.py
+++ discord_bot.py
@@ -2,6 +2,10 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+import pytesseract
+import cv2
+import numpy as np
+import re
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,5 +24,41 @@ async def say_hello(ctx):
     thread = await channel.create_thread(name='Receipts Thread')
     await thread.send('Welcome to the Receipts thread!')
     await thread.send('Please send your images here!')
+
+@bot.command(name='scan')
+async def scan_receipt(ctx):
+    message = ctx.message
+
+    try:
+        attachment = message.attachments[0]
+    except:
+        await ctx.send("Please attach a valid image file. EXCEPT")
+        return
+
+    if attachment.filename.lower().endswith(('.png', '.jpg', '.jpeg')) == False:
+        await ctx.send("Please attach a valid image file. ")
+        return
+    
+    response = await attachment.read()
+    nparr = np.frombuffer(response, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # Perform OCR on the image to extract the receipt total
+    text = pytesseract.image_to_string(img)
+    total = extract_total(text)
+
+    print(total)
+    
+    await ctx.send("Image scanned!")
+
+
+def extract_total(text):
+    # Search for a decimal number preceded by the word "total"
+    match = re.search(r'total\s*\$?(\d+\.\d+)', text, re.IGNORECASE)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
 
 bot.run(token)
